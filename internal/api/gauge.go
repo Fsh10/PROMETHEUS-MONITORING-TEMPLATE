0da21580ed716d175959cb0885501ece8a833992
fedor.shtab <fedor.shtab@gmail.com>
2022-07-27 17:22:34 +0400

    refactor: add gauge.go with initial implementation

---
+++ internal/api/gauge.go
@@ -0,0 +1,24 @@
+package api
+
+
+import (
+	"context"
+
+	pb "github.com/Fsh10/PROMETHEUS-MONITORING-TEMPLATE/pkg/metrics/github.com/Fsh10/PROMETHEUS-MONITORING-TEMPLATE"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"fmt"
+	"math/rand"
+
+)
+
+var (
+	gaugeCounter = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: "sandbox",
+			Name:      "rand_current",
+			Help:      "Current random value between 0 and 100",
+		},
+	)
+)
