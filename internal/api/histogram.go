1f7a8e723fdcecb8e7ce0f714a90f72ca9a19589
fedor.shtab <fedor.shtab@gmail.com>
2022-07-27 15:13:19 +0400

    chore: add histogram.go with initial implementation

---
+++ internal/api/histogram.go
@@ -0,0 +1,26 @@
+package api
+
+
+import (
+	"context"
+	"time"
+
+	pb "github.com/Fsh10/PROMETHEUS-MONITORING-TEMPLATE/pkg/metrics/github.com/Fsh10/PROMETHEUS-MONITORING-TEMPLATE"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"fmt"
+	"math/rand"
+
+)
+
+var (
+	requestDurationHistogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: "sandbox",
+			Name:       "request_duration_seconds",
+			Help:       "Histogram of request durations in seconds",
+			Buckets:    prometheus.DefBuckets,
+		},
+	)
+)
