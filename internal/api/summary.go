67e76730b92663ac637880a7c0e289e139aac31d
fedor.shtab <fedor.shtab@gmail.com>
2022-12-06 14:21:21 +0400

    chore: add summary.go with initial implementation

---
+++ internal/api/summary.go
@@ -0,0 +1,25 @@
+package api
+
+
+import (
+	"context"
+
+	pb "github.com/Fsh10/PROMETHEUS-MONITORING-TEMPLATE/pkg/metrics/github.com/Fsh10/PROMETHEUS-MONITORING-TEMPLATE"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"fmt"
+	"math/rand"
+
+)
+
+var (
+	requestSizeSummary = prometheus.NewSummary(
+		prometheus.SummaryOpts{
+			Namespace:  "sandbox",
+			Name:       "request_size_bytes",
+			Help:       "Summary of request sizes in bytes",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		},
+	)
+)
